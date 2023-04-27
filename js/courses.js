a41132a398b67a236e56d2c583809d729d65bed1
Andrij <andrij.pavlovich@gmail.com>
2023-04-27 21:14:58 +0300

    block courses js added

---
+++ js/courses.js
@@ -0,0 +1,11 @@
+const coursesBtncolor = document.querySelector('.courses__button-blink');
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ];
+let indexColor = 0;
+
+function updateColor() {
+    coursesBtncolor.style.background = colors[indexColor++];
+    indexColor = indexColor % colors.length;
+}
+
+let interval = setInterval(updateColor, 500);
+
