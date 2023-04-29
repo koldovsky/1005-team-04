3ce319e903c3ad976936a77a605583202ee1dada
Надія Посунько <129769719+NadiiaPosunko@users.noreply.github.com>
2023-04-29 16:52:15 +0000

    add js

--- js/header.partial.js
+++ js/header.partial.js
@@ -1,9 +1,35 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const headerMenu = document.querySelector('.header__menu');
+const headerTelephone = document.querySelector('.header__telephone-number');
+const headerSocial = document.querySelector('.social__logo-container');
+const headerLogo = document.querySelector('.logo-container');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     headerMenu.classList.toggle('active');
+    headerTelephone.classList.toggle('active');
+    headerSocial.classList.toggle('active');
+    headerLogo.classList.toggle('active');
 });
-burgerMenu.addEventListener('click', toggleMenu);
 
+const menuItems = document.querySelectorAll('.header__menu-item');
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    burgerMenu.classList.remove('active');
+    headerMenu.classList.remove('active');
+    headerTelephone.classList.remove('active');
+    headerSocial.classList.remove('active');
+    headerLogo.classList.remove('active');
+  });
+});
+
+const menuItemButton = document.querySelectorAll('.header__menu-item-button-top');
+menuItemButton.forEach(item => {
+  item.addEventListener('click', () => {
+    burgerMenu.classList.remove('active');
+    headerMenu.classList.remove('active');
+    headerTelephone.classList.remove('active');
+    headerSocial.classList.remove('active');
+    headerLogo.classList.remove('active');
+  });
+});
