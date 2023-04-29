f284cc4780ff075646262aa23bb47fc8e637dd07
Надія Посунько <129769719+NadiiaPosunko@users.noreply.github.com>
2023-04-29 18:00:11 +0000

    add css

--- js/header.partial.js
+++ js/header.partial.js
@@ -1,14 +1,10 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const headerMenu = document.querySelector('.header__menu');
-const headerTelephone = document.querySelector('.header__telephone-number');
-const headerSocial = document.querySelector('.social__logo-container');
 const headerLogo = document.querySelector('.logo-container');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     headerMenu.classList.toggle('active');
-    headerTelephone.classList.toggle('active');
-    headerSocial.classList.toggle('active');
     headerLogo.classList.toggle('active');
 });
 
@@ -17,8 +13,6 @@ menuItems.forEach(item => {
   item.addEventListener('click', () => {
     burgerMenu.classList.remove('active');
     headerMenu.classList.remove('active');
-    headerTelephone.classList.remove('active');
-    headerSocial.classList.remove('active');
     headerLogo.classList.remove('active');
   });
 });
@@ -28,8 +22,6 @@ menuItemButton.forEach(item => {
   item.addEventListener('click', () => {
     burgerMenu.classList.remove('active');
     headerMenu.classList.remove('active');
-    headerTelephone.classList.remove('active');
-    headerSocial.classList.remove('active');
     headerLogo.classList.remove('active');
   });
 });
