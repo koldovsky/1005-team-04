67422bb0537875c19ce8ccf715134bd9ba7615e2
GrigoriyKosarev <grigoriykosarev@gmail.com>
2023-05-08 19:48:05 +0000

    add js map

--- js/index.js
+++ js/index.js
@@ -4,4 +4,5 @@ document.addEventListener("partialsLoaded",() =>{
     import(`./header.partial.js`);
     import('./courses.js');
     import('./awords-carousel.js');
+    import('./google-map.js');
 });
